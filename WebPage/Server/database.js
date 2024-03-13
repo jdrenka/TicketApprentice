e6950658d31d8315835c9966af662e6dca781ec9
jdrenk <jpdrenka@gmail.com>
2024-03-13 14:16:54 -0700

    Dealt with merge conflicts.

--- WebPage/Server/database.js
+++ WebPage/Server/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2/promise');
 // MySQL database configuration
 const pool = mysql.createPool({
   connectionLimit: 10, // Maximum number of connections in the pool
-  host: 'alphabrainsdb.c1i22w4g43sf.us-west-1.rds.amazonaws.com',
-  user: 'admin',
+  host: '34.67.230.194',
+  user: 'root',
   password: 'alphabrainsdb',
   database: 'AlphaBrainsDB',
   waitForConnections: true,
